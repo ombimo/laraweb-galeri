c6cb13e7abe9e10f10267e442186d839ebab0a2b
Bimo Aji Pamungkas <bimo.aji.92@gmail.com>
2020-11-04 13:23:43 +0700

    add seo & breadcrumb

--- src/Controller/GaleriController.php
+++ src/Controller/GaleriController.php
@@ -4,12 +4,20 @@ namespace Ombimo\LarawebGaleri\Controller;
 
 use App\Http\Controllers\Controller;
 use Ombimo\LarawebGaleri\Models\GaleriAlbum;
+use Artesaos\SEOTools\Facades\SEOTools as SEO;
+use Ombimo\LarawebCore\Breadcrumb;
 
 class GaleriController extends Controller
 {
     public function index()
     {
         $dataAlbum = GaleriAlbum::with('cover')->paginate(10);
+
+        //breadcrumb
+        Breadcrumb::add('Gallery', route('galeri-index'));
+
+        //seo
+        SEO::setTitle('Gallery');
 
         return view('galeri.index', [
             'dataAlbum' => $dataAlbum,
--- src/Controller/GaleriDetailController.php
+++ src/Controller/GaleriDetailController.php
@@ -4,12 +4,22 @@ namespace Ombimo\LarawebGaleri\Controller;
 
 use App\Http\Controllers\Controller;
 use Ombimo\LarawebGaleri\Models\GaleriAlbum;
+use Artesaos\SEOTools\Facades\SEOTools as SEO;
+use Ombimo\LarawebCore\Breadcrumb;
 
 class GaleriDetailController extends Controller
 {
     public function get($id, $slug = null)
     {
         $album = GaleriAlbum::with('data')->findOrFail($id);
+
+        //breadcrumb
+        Breadcrumb::add('Gallery', route('galeri-index'));
+        Breadcrumb::add($album->nama, $album->link_detail);
+
+        //seo
+        SEO::setTitle($album->nama . ' - Gallery');
+        SEO::setCanonical($album->link_detail);
 
         return view('galeri.detail', [
             'album' => $album,
